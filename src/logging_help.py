e3d9c2fc639bbe9fd28bf69f8c451776d6b06452
Nathaniel Beckstead <becksteadn@gmail.com>
2020-02-05 13:33:07 -0500

    debug

--- src/logging_help.py
+++ src/logging_help.py
@@ -57,6 +57,7 @@ def get_secret():
     else:
         # Decrypts secret using the associated KMS CMK.
         # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        print(get_secret_value_response)
         if 'SecretString' in get_secret_value_response:
             secret = get_secret_value_response['SecretString']
         else:
